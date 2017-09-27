efa08dbb70a2282413aabd6ae1f901315c2d3f53
mattosaurus <mattosaurus@outlook.com>
2017-09-27 10:14:15 +0100

    Added derived zoom options.

---
+++ src/ChartJSCore/Plugins/Zoom/ZoomPieOptions.cs
@@ -0,0 +1,18 @@
+﻿using ChartJSCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChartJSCore.Plugins.Zoom
+{
+    /// <summary>
+    /// Requires Zoom and Pan plugin.
+    /// https://github.com/chartjs/chartjs-plugin-zoom
+    /// </summary>
+    public class ZoomPieOptions : PieOptions
+    {
+        public Pan Pan { get; set; }
+
+        public Zoom Zoom { get; set; }
+    }
+}
