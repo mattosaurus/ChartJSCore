51481455ea180dd672ce94a87308d44e160bbfaf
mattosaurus <mattosaurus@outlook.com>
2017-09-27 09:37:33 +0100

    Updated summary.

--- src/ChartJSCore/Plugins/Zoom/ZoomOptions.cs
+++ src/ChartJSCore/Plugins/Zoom/ZoomOptions.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace ChartJSCore.Plugins.Zoom
 {
+    /// <summary>
+    /// Requires Zoom and Pan plugin.
+    /// https://github.com/chartjs/chartjs-plugin-zoom
+    /// </summary>
     public class ZoomOptions : Models.Options
     {
         public Pan Pan { get; set; }
