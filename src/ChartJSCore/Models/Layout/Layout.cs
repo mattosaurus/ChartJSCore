64e1c6ab8f10c3f657dc14f517ee730225bd2e4a
Mattosaurus <mattosaurus@outlook.com>
2018-07-02 21:57:04 +0100

    Renamed to padding to prevent breakage.

--- src/ChartJSCore/Models/Layout/Layout.cs
+++ src/ChartJSCore/Models/Layout/Layout.cs
@@ -12,7 +12,7 @@ namespace ChartJSCore.Models
         /// The padding to add inside the ChartJSCore. If this value is a number, it is applied to all sides of the chart (left, top, right, bottom).
         /// </summary>
         [JsonProperty("padding")]
-        public int? PaddingInt { get; set; }
+        public int? Padding { get; set; }
 
         /// <summary>
         /// The padding to add inside the ChartJSCore. If this value is an object, the left property defines the left padding. Similarly the right, top, and bottom properties can also be specified.
