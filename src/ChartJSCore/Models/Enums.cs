baf15123cb0b1fcf1bd73bf21bb42dad90ec0ae9
David <davidhodel6@gmail.com>
2019-03-15 21:50:58 +0100

    Introduce enum values for the type of chart

---
+++ src/ChartJSCore/Models/Enums.cs
@@ -0,0 +1,14 @@
+﻿namespace ChartJSCore.Models
+{
+    public static class Enums
+    {
+        public enum ChartType : byte
+        {
+            BarChart = 0,
+            BubbleChart = 1,
+            RadarChart = 2,
+            PolarAreaChart = 3,
+            PieChart = 4
+        }
+    }
+}
