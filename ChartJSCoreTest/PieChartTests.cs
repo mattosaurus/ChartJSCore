5412aee94212bdb2b8ea59b3552518904f689906
David <davidhodel6@gmail.com>
2019-07-02 19:06:40 +0200

    Converted console application to real unit tests

---
+++ ChartJSCoreTest/PieChartTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using ChartJSCore.Models;
+using NUnit.Framework;
+
+namespace ChartJSCoreTest
+{
+    [TestFixture]
+    public class PieChartTests
+    {
+        private const string KNOWN_GOOD_CHART = "var pieChartElement = document.getElementById(\"pieChart\");\r\nvar pieChart = new Chart(pieChartElement, {\"type\":\"pie\",\"data\":{\"datasets\":[{\"type\":\"pie\",\"backgroundColor\":[\"#FF6384\",\"#36A2EB\",\"#FFCE56\"],\"hoverBackgroundColor\":[\"#FF6384\",\"#36A2EB\",\"#FFCE56\"],\"data\":[300.0,50.0,100.0],\"label\":\"My dataset\"}],\"labels\":[\"Red\",\"Blue\",\"Yellow\"]},\"options\":{}}\r\n);";
+
+        [Test]
+        public void Generate_PieChart_Generates_Valid_Chart()
+        {
+            string expected = KNOWN_GOOD_CHART;
+
+            string actual = GeneratePieChart().CreateChartCode("pieChart");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static Chart GeneratePieChart()
+        {
+            var chart = new Chart { Type = Enums.ChartType.Pie };
+
+            var data = new Data { Labels = new List<string> { "Red", "Blue", "Yellow" } };
+
+            var dataset = new PieDataset
+            {
+                Label = "My dataset",
+                BackgroundColor = new List<string> { "#FF6384", "#36A2EB", "#FFCE56" },
+                HoverBackgroundColor = new List<string> { "#FF6384", "#36A2EB", "#FFCE56" },
+                Data = new List<double> { 300, 50, 100 }
+            };
+
+            data.Datasets = new List<Dataset> { dataset };
+
+            chart.Data = data;
+
+            return chart;
+        }
+    }
+
+}
---
+++ ChartJSCoreTest/RadarChartTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using ChartJSCore.Models;
+using NUnit.Framework;
+
+namespace ChartJSCoreTest
+{
+    [TestFixture]
+    public class RadarChartTests
+    {
+        private const string KNOWN_GOOD_CHART = "var radarChartElement = document.getElementById(\"radarChart\");\r\nvar radarChart = new Chart(radarChartElement, {\"type\":\"radar\",\"data\":{\"datasets\":[{\"type\":\"radar\",\"backgroundColor\":\"rgba(179,181,198,0.2)\",\"borderColor\":\"rgba(179,181,198,1)\",\"pointBorderColor\":\"#fff\",\"pointBackgroundColor\":\"rgba(179,181,198,1)\",\"pointHoverBackgroundColor\":\"#fff\",\"pointHoverBorderColor\":\"rgba(179,181,198,1)\",\"data\":[65.0,59.0,80.0,81.0,56.0,55.0,40.0],\"label\":\"My First dataset\"},{\"type\":\"radar\",\"backgroundColor\":\"rgba(255,99,132,0.2)\",\"borderColor\":\"rgba(255,99,132,1)\",\"pointBorderColor\":\"#fff\",\"pointBackgroundColor\":\"rgba(255,99,132,1)\",\"pointHoverBackgroundColor\":\"#fff\",\"pointHoverBorderColor\":\"rgba(255,99,132,1)\",\"data\":[28.0,48.0,40.0,19.0,96.0,27.0,100.0],\"label\":\"My Second dataset\"}],\"labels\":[\"Eating\",\"Drinking\",\"Sleeping\",\"Designing\",\"Coding\",\"Cycling\",\"Running\"]},\"options\":{}}\r\n);";
+
+        [Test]
+        public void Generate_RadarChart_Generates_Valid_Chart()
+        {
+            string expected = KNOWN_GOOD_CHART;
+
+            string actual = GenerateRadarChart().CreateChartCode("radarChart");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static Chart GenerateRadarChart()
+        {
+            var chart = new Chart { Type = Enums.ChartType.Radar };
+
+            var data = new Data
+            {
+                Labels = new List<string>
+                {
+                    "Eating",
+                    "Drinking",
+                    "Sleeping",
+                    "Designing",
+                    "Coding",
+                    "Cycling",
+                    "Running"
+                }
+            };
+
+            var dataset1 = new RadarDataset
+            {
+                Label = "My First dataset",
+                BackgroundColor = "rgba(179,181,198,0.2)",
+                BorderColor = "rgba(179,181,198,1)",
+                PointBackgroundColor = new List<string> { "rgba(179,181,198,1)" },
+                PointBorderColor = new List<string> { "#fff" },
+                PointHoverBackgroundColor = new List<string> { "#fff" },
+                PointHoverBorderColor = new List<string> { "rgba(179,181,198,1)" },
+                Data = new List<double> { 65, 59, 80, 81, 56, 55, 40 }
+            };
+
+            var dataset2 = new RadarDataset
+            {
+                Label = "My Second dataset",
+                BackgroundColor = "rgba(255,99,132,0.2)",
+                BorderColor = "rgba(255,99,132,1)",
+                PointBackgroundColor = new List<string> { "rgba(255,99,132,1)" },
+                PointBorderColor = new List<string> { "#fff" },
+                PointHoverBackgroundColor = new List<string> { "#fff" },
+                PointHoverBorderColor = new List<string> { "rgba(255,99,132,1)" },
+                Data = new List<double> { 28, 48, 40, 19, 96, 27, 100 }
+            };
+
+            data.Datasets = new List<Dataset> { dataset1, dataset2 };
+
+            chart.Data = data;
+
+            return chart;
+        }
+    }
+
+}
