5e5d936d6cface6845d4598b5c13e0439d1657c0
Matthew Grant <matthew.grant@planning-inc.co.uk>
2021-08-27 16:08:20 +0100

    Update for font

--- src/ChartJSCore/Models/Options/Title/Title.cs
+++ src/ChartJSCore/Models/Options/Title/Title.cs
@@ -5,39 +5,19 @@ namespace ChartJSCore.Models
     public class Title : Base
     {
         /// <summary>
-        /// Display the title block.
+        /// Color of text.
+        /// </summary>
+        public ChartColor Color { get; set; }
+
+        /// <summary>
+        /// Is the legend title displayed.
         /// </summary>
         public bool? Display { get; set; }
 
         /// <summary>
-        /// Position of the title. Possible values are 'top', 'left', 'bottom' and 'right'.
+        /// Font of the title.
         /// </summary>
-        public string Position { get; set; }
-
-        /// <summary>
-        /// Marks that this box should take the full width of the canvas (pushing down other boxes).
-        /// </summary>
-        public bool? FullWidth { get; set; }
-
-        /// <summary>
-        /// Font size inherited from global configuration.
-        /// </summary>
-        public int? FontSize { get; set; }
-
-        /// <summary>
-        /// Font family inherited from global configuration.
-        /// </summary>
-        public string FontFamily { get; set; }
-
-        /// <summary>
-        /// Font color inherited from global configuration.
-        /// </summary>
-        public ChartColor FontColor { get; set; }
-
-        /// <summary>
-        /// Font styling of the title.
-        /// </summary>
-        public string FontStyle { get; set; }
+        public Font Font { get; set; }
 
         /// <summary>
         /// Number of pixels to add above and below the title text.
