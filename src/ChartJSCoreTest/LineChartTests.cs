50aad30c097a720bd16b749ac8413bc9df76f313
bcanteneurEiffage <68943107+bcanteneurEiffage@users.noreply.github.com>
2022-01-11 10:02:00 +0100

    Working LineChartTests

--- src/ChartJSCoreTest/LineChartTests.cs
+++ src/ChartJSCoreTest/LineChartTests.cs
@@ -8,7 +8,7 @@ namespace ChartJSCoreTest
     [TestFixture]
     public class LineChartTests
     {
-        private const string KNOWN_GOOD_CHART = "var lineChartElement = document.getElementById(\"lineChart\");\r\nvar lineChart = new Chart(lineChartElement, {\"type\":\"line\",\"data\":{\"datasets\":[{\"type\":\"line\",\"fill\":false,\"lineTension\":0.1,\"backgroundColor\":\"rgba(75, 192, 192, 0.4)\",\"borderColor\":\"rgba(75, 192, 192, 1)\",\"borderCapStyle\":\"butt\",\"borderDash\":[],\"borderDashOffset\":0.0,\"borderJoinStyle\":\"miter\",\"pointBorderColor\":\"rgba(75, 192, 192, 1)\",\"pointBackgroundColor\":\"rgba(255, 255, 255, 1)\",\"pointBorderWidth\":1,\"pointRadius\":1,\"pointHoverRadius\":5,\"pointHitRadius\":10,\"pointHoverBackgroundColor\":\"rgba(75, 192, 192, 1)\",\"pointHoverBorderColor\":\"rgba(220, 220, 220, 1)\",\"pointHoverBorderWidth\":2,\"spanGaps\":false,\"data\":[65.0,59.0,80.0,81.0,56.0,55.0,40.0],\"label\":\"My First dataset\"}],\"labels\":[\"January\",\"February\",\"March\",\"April\",\"May\",\"June\",\"July\"]},\"options\":{\"scales\":{\"y\":{\"ticks\":{\"callback\":function(value, index, values) {return '$' + value;}}}}}}\r\n);";
+        private const string KNOWN_GOOD_CHART = "var lineChartElement = document.getElementById(\"lineChart\");\r\nvar lineChart = new Chart(lineChartElement, {\"type\":\"line\",\"data\":{\"datasets\":[{\"type\":\"line\",\"fill\":{\"target\":\"origin\",\"above\":\"rgba(255, 0, 0, 0.3)\",\"below\":\"rgba(0, 255, 0, 0.3)\"},\"lineTension\":0.1,\"backgroundColor\":\"rgba(75, 192, 192, 0.4)\",\"borderColor\":\"rgba(75, 192, 192, 1)\",\"borderCapStyle\":\"butt\",\"borderDash\":[],\"borderDashOffset\":0.0,\"borderJoinStyle\":\"miter\",\"pointBorderColor\":\"rgba(75, 192, 192, 1)\",\"pointBackgroundColor\":\"rgba(255, 255, 255, 1)\",\"pointBorderWidth\":1,\"pointRadius\":1,\"pointHoverRadius\":5,\"pointHitRadius\":10,\"pointHoverBackgroundColor\":\"rgba(75, 192, 192, 1)\",\"pointHoverBorderColor\":\"rgba(220, 220, 220, 1)\",\"pointHoverBorderWidth\":2,\"spanGaps\":false,\"stepped\":\"middle\",\"label\":\"My First dataset\",\"data\":[65.0,59.0,80.0,81.0,56.0,55.0,40.0]}],\"labels\":[\"January\",\"February\",\"March\",\"April\",\"May\",\"June\",\"July\"]},\"options\":{\"scales\":{\"y\":{\"ticks\":{\"callback\":function(value, index, values) {return '$' + value;}}}}}}\r\n);";
 
         [Test]
         public void Generate_LineChart_Generates_Valid_Chart()
@@ -60,7 +60,8 @@ namespace ChartJSCoreTest
                 PointHoverBorderWidth = new List<int> { 2 },
                 PointRadius = new List<int> { 1 },
                 PointHitRadius = new List<int> { 10 },
-                SpanGaps = false
+                SpanGaps = false,
+                Stepped = "middle"
             };
 
             data.Datasets = new List<Dataset> { dataset };
