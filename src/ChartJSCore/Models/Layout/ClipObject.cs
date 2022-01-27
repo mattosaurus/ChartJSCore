36179cf48a3575f3a7d50d85b077f287ebeeae83
mattosaurus <bitscry@outlook.com>
2022-01-27 15:11:14 +0000

    Update line to v3

---
+++ src/ChartJSCore/Models/Layout/ClipObject.cs
@@ -0,0 +1,13 @@
+﻿namespace ChartJSCore.Models
+{
+    public class ClipObject : Base
+    {
+        public int? Left { get; set; }
+
+        public int? Right { get; set; }
+
+        public int? Top { get; set; }
+
+        public int? Bottom { get; set; }
+    }
+}
--- src/ChartJSCore/Models/Line/LineDataset.cs
+++ src/ChartJSCore/Models/Line/LineDataset.cs
@@ -11,6 +11,29 @@ namespace ChartJSCore.Models
         /// </summary>
         public Enums.ChartType Type { get; set; } = Enums.ChartType.Line;
 
+        #region General
+        // https://www.chartjs.org/docs/latest/charts/line.html#general
+        /// <summary>
+        /// How to clip relative to chartArea. Positive value allows overflow, negative value clips that many pixels inside chartArea. 0 = clip at chartArea. Clipping can also be configured per side: clip: {left: 5, top: false, right: -2, bottom: 0}
+        /// </summary>
+        [JsonConverter(typeof(ClipConverter))]
+        public Clip Clip { get; set; }
+
+        /// <summary>
+        /// The base axis of the dataset. 'x' for horizontal lines and 'y' for vertical lines.
+        /// </summary>
+        public string IndexAxis { get; set; }
+
+        /// <summary>
+        /// The drawing order of dataset. Also affects order for stacking, tooltip and legend.
+        /// </summary>
+        public int Order { get; set; }
+
+        /// <summary>
+        /// The ID of the group to which this dataset belongs to (when stacked, each group will be a separate stack).
+        /// </summary>
+        public string Stack { get; set; }
+
         /// <summary>
         /// The ID of the x axis to plot this dataset on.
         /// </summary>
@@ -20,42 +43,74 @@ namespace ChartJSCore.Models
         /// The ID of the y axis to plot this dataset on.
         /// </summary>
         public string YAxisID { get; set; }
+        #endregion General
+
+        #region PointStyling
+        // https://www.chartjs.org/docs/latest/charts/line.html#point-styling
+        /// <summary>
+        /// The fill color for points.
+        /// </summary>
+        [JsonConverter(typeof(SingleOrArrayConverter<ChartColor>))]
+        public IList<ChartColor> PointBackgroundColor { get; set; }
 
         /// <summary>
-        /// If true, fill the area under the line.
+        /// The border color for points.
         /// </summary>
-        [JsonConverter(typeof(BoolIntStringConverter))]
-        public string Fill { get; set; }
+        [JsonConverter(typeof(SingleOrArrayConverter<ChartColor>))]
+        public IList<ChartColor> PointBorderColor { get; set; }
 
         /// <summary>
-        /// Algorithm used to interpolate a smooth curve from the discrete data points. Options are 'default' and 'monotone'. The 'default' algorithm uses a custom weighted cubic interpolation, which produces pleasant curves for all types of datasets. The 'monotone' algorithm is more suited to y = f(x) datasets : it preserves monotonicity (or piecewise monotonicity) of the dataset being interpolated, and ensures local extremums (if any) stay at input data points. If left untouched (undefined), the global options.elements.line.cubicInterpolationMode property is used.
+        /// The width of the point border in pixels.
         /// </summary>
-        public string CubicInterpolationMode { get; set; }
+        [JsonConverter(typeof(SingleOrArrayConverter<int>))]
+        public IList<int> PointBorderWidth { get; set; }
 
         /// <summary>
-        /// Bezier curve tension of the line. Set to 0 to draw straightlines. This option is ignored if monotone cubic interpolation is used. Note This was renamed from 'tension' but the old name still works.
+        /// The pixel size of the non-displayed point that reacts to mouse events.
         /// </summary>
-        public double? LineTension { get; set; }
+        [JsonConverter(typeof(SingleOrArrayConverter<int>))]
+        public IList<int> PointHitRadius { get; set; }
 
+        /// <summary>
+        /// The radius of the point shape. If set to 0, nothing is rendered.
+        /// </summary>
+        [JsonConverter(typeof(SingleOrArrayConverter<int>))]
+        public IList<int> PointRadius { get; set; }
+
+        /// <summary>
+        /// The rotation of the point in degrees.
+        /// </summary>
+        public int? PointRotation { get; set; }
+
+        // TODO: Allow images as well as strings.
+        /// <summary>
+        /// The style of point. Options are 'circle', 'triangle', 'rect', 'rectRot', 'cross', 'crossRot', 'star', 'line', and 'dash'. If the option is an image, that image is drawn on the canvas using drawImage.
+        /// </summary>
+        [JsonConverter(typeof(SingleOrArrayConverter<string>))]
+        public IList<string> PointStyle { get; set; }
+
+        /// <summary>
+        /// Draw the active points of a dataset over the other points of the dataset
+        /// </summary>
+        public bool? DrawActiveElementsOnTop { get; set; }
+        #endregion PointStyling
+
+        #region LineStyling
+        // https://www.chartjs.org/docs/latest/charts/line.html#line-styling
         /// <summary>
         /// The fill color under the line.
         /// </summary>
         public ChartColor BackgroundColor { get; set; }
 
         /// <summary>
-        /// The width of the line in pixels.
+        /// Cap style of the line.
         /// </summary>
-        public int? BorderWidth { get; set; }
+        public string BorderCapStyle { get; set; }
 
         /// <summary>
         /// The color of the line.
         /// </summary>
         public ChartColor BorderColor { get; set; }
-
-        /// <summary>
-        /// Cap style of the line.
-        /// </summary>
-        public string BorderCapStyle { get; set; }
 
         /// <summary>
         /// Length and spacing of dashes.
@@ -73,65 +128,20 @@ namespace ChartJSCore.Models
         public string BorderJoinStyle { get; set; }
 
         /// <summary>
-        /// The border color for points.
+        /// The width of the line in pixels.
         /// </summary>
-        [JsonConverter(typeof(SingleOrArrayConverter<ChartColor>))]
-        public IList<ChartColor> PointBorderColor { get; set; }
+        public int? BorderWidth { get; set; }
 
         /// <summary>
-        /// The fill color for points.
+        /// If true, fill the area under the line.
         /// </summary>
-        [JsonConverter(typeof(SingleOrArrayConverter<ChartColor>))]
-        public IList<ChartColor> PointBackgroundColor { get; set; }
+        [JsonConverter(typeof(BoolIntStringConverter))]
+        public string Fill { get; set; }
 
         /// <summary>
-        /// The width of the point border in pixels.
+        /// Bezier curve tension of the line. Set to 0 to draw straightlines. This option is ignored if monotone cubic interpolation is used. Note This was renamed from 'tension' but the old name still works.
         /// </summary>
-        [JsonConverter(typeof(SingleOrArrayConverter<int>))]
-        public IList<int> PointBorderWidth { get; set; }
-
-        /// <summary>
-        /// The radius of the point shape. If set to 0, nothing is rendered.
-        /// </summary>
-        [JsonConverter(typeof(SingleOrArrayConverter<int>))]
-        public IList<int> PointRadius { get; set; }
-
-        /// <summary>
-        /// The radius of the point when hovered.
-        /// </summary>
-        [JsonConverter(typeof(SingleOrArrayConverter<int>))]
-        public IList<int> PointHoverRadius { get; set; }
-
-        /// <summary>
-        /// The pixel size of the non-displayed point that reacts to mouse events.
-        /// </summary>
-        [JsonConverter(typeof(SingleOrArrayConverter<int>))]
-        public IList<int> PointHitRadius { get; set; }
-
-        /// <summary>
-        /// Point background color when hovered.
-        /// </summary>
-        [JsonConverter(typeof(SingleOrArrayConverter<ChartColor>))]
-        public IList<ChartColor> PointHoverBackgroundColor { get; set; }
-
-        /// <summary>
-        /// Point border color when hovered.
-        /// </summary>
-        [JsonConverter(typeof(SingleOrArrayConverter<ChartColor>))]
-        public IList<ChartColor> PointHoverBorderColor { get; set; }
-
-        /// <summary>
-        /// Border width of point when hovered.
-        /// </summary>
-        [JsonConverter(typeof(SingleOrArrayConverter<int>))]
-        public IList<int> PointHoverBorderWidth { get; set; }
-
-        // TODO: Allow images as well as strings.
-        /// <summary>
-        /// The style of point. Options are 'circle', 'triangle', 'rect', 'rectRot', 'cross', 'crossRot', 'star', 'line', and 'dash'. If the option is an image, that image is drawn on the canvas using drawImage.
-        /// </summary>
-        [JsonConverter(typeof(SingleOrArrayConverter<string>))]
-        public IList<string> PointStyle { get; set; }
+        public double? Tension { get; set; }
 
         /// <summary>
         /// If false, the line is not drawn for this dataset.
@@ -139,14 +149,55 @@ namespace ChartJSCore.Models
         public bool? ShowLine { get; set; }
 
         /// <summary>
-        /// If true, lines will be drawn between points with no or null data.
+        /// If true, lines will be drawn between points with no or null data. If false, points with null data will create a break in the line. Can also be a number specifying the maximum gap length to span. The unit of the value depends on the scale used.
         /// </summary>
         public bool? SpanGaps { get; set; }
+        #endregion LineStyling
+
+        #region Interactions
+        // https://www.chartjs.org/docs/latest/charts/line.html#interactions
+        /// <summary>
+        /// Point background color when hovered.
+        /// </summary>
+        public ChartColor PointHoverBackgroundColor { get; set; }
 
         /// <summary>
-        /// If true, the line is shown as a stepped line and 'lineTension' will be ignored.
+        /// Point border color when hovered.
         /// </summary>
-        [JsonConverter(typeof(BoolIntStringConverter))]
+        public ChartColor PointHoverBorderColor { get; set; }
+
+        /// <summary>
+        /// Border width of point when hovered.
+        /// </summary>
+        public int? PointHoverBorderWidth { get; set; }
+
+        /// <summary>
+        /// The radius of the point when hovered.
+        /// </summary>
+        public int? PointHoverRadius { get; set; }
+        #endregion Interactions
+
+        #region CubicInterpolationMode
+        // https://www.chartjs.org/docs/latest/charts/line.html#cubicinterpolationmode
+        /// <summary>
+        /// Algorithm used to interpolate a smooth curve from the discrete data points. Options are 'default' and 'monotone'. The 'default' algorithm uses a custom weighted cubic interpolation, which produces pleasant curves for all types of datasets. The 'monotone' algorithm is more suited to y = f(x) datasets : it preserves monotonicity (or piecewise monotonicity) of the dataset being interpolated, and ensures local extremums (if any) stay at input data points. If left untouched (undefined), the global options.elements.line.cubicInterpolationMode property is used.
+        /// </summary>
+        public string CubicInterpolationMode { get; set; }
+        #endregion CubicInterpolationMode
+
+        #region Stepped
+        // https://www.chartjs.org/docs/latest/charts/line.html#stepped
+        /// <summary>
+        /// The following values are supported for stepped.
+        /// false: No Step Interpolation (default)
+        /// true: Step-before Interpolation(eq. 'before')
+        /// 'before': Step-before Interpolation
+        /// 'after': Step-after Interpolation
+        /// 'middle': Step-middle Interpolation
+        /// If the stepped value is set to anything other than false, tension will be ignored.
+        /// </summary>
+        [JsonConverter(typeof(BoolStringConverter))]
         public string SteppedLine { get; set; }
+        #endregion Stepped
     }
 }
