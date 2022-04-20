fd99e0c34c062c68dd48ce9730c42f4cfec4098c
mattosaurus <mattosaurus@outlook.com>
2022-04-20 16:29:42 +0100

    Add demo

---
+++ ChartJSCore.Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChartJSCore.Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
