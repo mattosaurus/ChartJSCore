4bfd68f35869b27777f06e052cd8ad8bca67ff1e
mattosaurus <mattosaurus@outlook.com>
2022-04-20 16:30:22 +0100

    Add vertical bar datapoints

---
+++ src/ChartJSCore/Models/Bar/VerticalBarDataPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChartJSCore.Models
+{
+    public class VerticalBarDataPoint
+    {
+        public VerticalBarDataPoint(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
+    }
+}
---
+++ src/ChartJSCore/Models/Bar/VerticalBarDataset.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChartJSCore.Models
+{
+    public class VerticalBarDataset : BarDataset
+    {
+        public new IList<VerticalBarDataPoint> Data { get; set; }
+    }
+}
