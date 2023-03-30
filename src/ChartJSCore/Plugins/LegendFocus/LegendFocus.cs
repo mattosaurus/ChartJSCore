0906574eae496566ece75a97d587492144697968
Stephan <stephan.stapel@web.de>
2023-03-30 21:15:04 +0200

    added a plugin for toggling the legend behaviour
    In chart.js, clicking on the legend hides this legend. However, the average user is used to the oppositive behaviour. Using this code (grabbed from stackoverflow), we can focus on a particular dataset when clicking on the corresponding legend element.
    
    Please note that it was only tested with line charts!

---
+++ src/ChartJSCore/Plugins/LegendFocus/LegendFocus.cs
@@ -0,0 +1,42 @@
+﻿using ChartJSCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChartJSCore.Plugins.LegenFocus
+{
+    /// <summary>
+    /// Code was copied from here:
+    /// https://stackoverflow.com/questions/42555513/chart-js-change-legend-toggle-behaviour
+    /// </summary>
+    public class LegendFocus : Legend
+    {
+        public LegendFocus()
+        {
+            Display = true;
+            OnClick = @"function(e, legendItem) {
+                                    var index = legendItem.datasetIndex;
+                                    var ci = this.chart;
+                                    var alreadyHidden = (ci.getDatasetMeta(index).hidden === null) ? false : ci.getDatasetMeta(index).hidden;
+
+                                    ci.data.datasets.forEach(function(e, i) {
+                                        var meta = ci.getDatasetMeta(i);
+
+                                        if (i !== index) {
+                                            if (!alreadyHidden) {
+                                                meta.hidden = meta.hidden === null ? !meta.hidden : null;
+                                            } 
+                                            else if (meta.hidden === null) 
+                                            {
+                                                meta.hidden = true;
+                                            }
+                                        }
+                                        else if (i === index) {
+                                            meta.hidden = null;
+                                        }
+                                    });
+                                    ci.update();
+                                }";
+        }
+    }
+}
