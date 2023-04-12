14ce2f8d23e8bab1513f82119558afff14b06b4e
Tom Halter <email@tomhalter.com>
2023-04-12 15:54:51 -0400

    Add support for floating bar charts

---
+++ src/ChartJSCore/Models/Bar/FloatingBarDataset.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ChartJSCore.Models
+{
+    // https://www.chartjs.org/docs/latest/samples/bar/floating.html
+    /// <summary>
+    /// Dataset for a Floating Bar chart
+    /// </summary>
+    public class FloatingBarDataset : BarDataset
+    {
+        /// <summary>
+        /// The data to plot. Must be in the form of a List containing pairs of values (lower and upper value) for each bar.
+        /// </summary>
+        public new List<List<double>> Data { get; set; }
+    }
+}
