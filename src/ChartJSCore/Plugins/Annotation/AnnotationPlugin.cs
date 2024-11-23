873c82bdc73f7a8549cf56270d16097baba3476b
Stephan <stephan.stapel@web.de>
2024-11-23 10:48:03 +0100

    Added annotation plugin
    see https://www.chartjs.org/chartjs-plugin-annotation/latest/

---
+++ src/ChartJSCore/Plugins/Annotation/AnnotationPlugin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChartJSCore.Plugins.Annotation
+{
+    public class AnnotationPlugin
+    {
+        public Dictionary<string, ChartJSCore.Plugins.Annotation.Annotation> Annotations { get; set; } = new Dictionary<string, Annotation>();
+    }
+}
---
+++ src/ChartJSCore/Plugins/Annotation/LabelAnnotation.cs
@@ -0,0 +1,23 @@
+﻿using ChartJSCore.Helpers;
+using ChartJSCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChartJSCore.Plugins.Annotation
+{
+    /// <summary>
+    ///  Label annotations are used to add contents on the chart area. This can be useful for describing values that are of interest.
+    /// </summary>
+    public class LabelAnnotation : Annotation
+    {
+        public string Type { get; private set; } = "label";
+        public decimal? xValue { get; set; }
+        public decimal? yValue { get; set; }
+        public decimal? Height { get; set; }
+        public decimal? Width { get; set; }
+        public List<string> Content { get; set; } = new List<string>();
+        public Font Font { get; set; }
+        public ChartColor Color { get; set; } = ChartColor.FromRgba(0, 0, 0, 1);        
+    }
+}
